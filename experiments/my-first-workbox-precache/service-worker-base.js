fcd3e3d67ae56a574e5c89c0c8e8fb5cde5f6ed7
Dan Webb <mail@danwebb.co>
2020-03-25 09:09:52 +0000

    feat: Caching content

--- experiments/my-first-workbox-precache/service-worker-base.js
+++ experiments/my-first-workbox-precache/service-worker-base.js
@@ -1,3 +1,7 @@
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.0.0/workbox-sw.js');
 
 workbox.precaching.precacheAndRoute(self.__WB_MANIFEST);
+
+self.addEventListener('install', () => {
+  self.skipWaiting();
+});
