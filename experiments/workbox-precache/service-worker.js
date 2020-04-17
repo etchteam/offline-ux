f50e9b4a8707f52bad19c128d1106729f313a87a
Dan Webb <mail@danwebb.co>
2020-04-17 09:12:12 +0100

    feat: Versioning offline content

--- experiments/workbox-precache/service-worker.js
+++ experiments/workbox-precache/service-worker.js
@@ -1,3 +1,11 @@
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.0.0/workbox-sw.js');
 
-workbox.precaching.precacheAndRoute([{"revision":"13187a99c3b92698b93520df29dc23e4","url":"image.jpeg"},{"revision":"aa274b8988a81f715077e104a6753dac","url":"index.html"},{"revision":"5e89a0d14a7ef2ccb7ae86621bd1c84e","url":"styles.css"}]);
+workbox.precaching.precacheAndRoute([
+  {"revision":"13187a99c3b92698b93520df29dc23e4","url":"image.jpeg"},
+  {"revision":"a9b2611a124faa588cee9149e1825eeb","url":"index.html"},
+  {"revision":"5e89a0d14a7ef2ccb7ae86621bd1c84e","url":"styles.css"}
+]);
+
+self.addEventListener('install', () => {
+  self.skipWaiting();
+});
