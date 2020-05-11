3759a82ed64b2e2456ff3ee8e5e323816d72e34f
Dan Webb <mail@danwebb.co>
2020-05-11 10:26:45 +0100

    feat: Offline fallback demo

--- experiments/workbox-precache/rollup.config.js
+++ experiments/workbox-precache/rollup.config.js
@@ -2,18 +2,24 @@ const resolve = require('@rollup/plugin-node-resolve');
 const replace = require('rollup-plugin-replace');
 const { terser } = require('rollup-plugin-terser');
 const { injectManifest } = require('rollup-plugin-workbox');
-const workboxConfig = require('./workbox-config.js')
+const workboxConfig = require('./workbox-config.js');
 
-export default {
+export default [{
   input: 'service-worker-template.js',
   output: {
     dir: 'src',
     format: 'cjs'
   },
+  plugins: [injectManifest(workboxConfig)]
+}, {
+  input: 'service-worker.js',
+  output: {
+    dir: 'src',
+    format: 'cjs'
+  },
   plugins: [
-    injectManifest(workboxConfig),
     resolve(),
     replace({ 'process.env.NODE_ENV': JSON.stringify('production') }),
     terser()
   ]
-};
+}];
